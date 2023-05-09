bf728ab52ec424a6eec7851436c05b3f76785ea7
ymk2018 <makingtheroad2650@gmail.com>
2023-05-09 23:39:47 +0900

    modified

--- feature_1.py
+++ feature_1.py
@@ -4,7 +4,7 @@ import pytz
 def main():
     now = datetime.now(tz=pytz.timezone('Asia/Tokyo'))
     print(now.strftime('%Y/%m/%d %H:%M:%S'))
-    print(now.strftime('%y/%m/%d'))
+    print(now.strftime('%Y'))
     
 
 if __name__ == '__main__':
