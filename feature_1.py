a94c573cf41721321b0dc07d46f66f2a6ddcc8b3
ymk2018 <makingtheroad2650@gmail.com>
2023-05-09 23:47:36 +0900

    comment out print

--- feature_1.py
+++ feature_1.py
@@ -4,7 +4,7 @@ import pytz
 def main():
     now = datetime.now(tz=pytz.timezone('Asia/Tokyo'))
     print(now.strftime('%Y/%m/%d %H:%M:%S'))
-    print('add this line')
+    # print('add this line')
     print(now.strftime('%Y/%m'))
     
 
